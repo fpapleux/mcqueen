6eaae2be7e6e25d92963bf46318309300bc278c6
fabien <fabien@pi2.(none)>
2014-03-15 12:23:28 -0400

    new file:   a.out 	new file:   test.cpp 	deleted:    main.cpp

---
+++ gpio/test.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <wiringPi.h>
+#include "gpio.h"
+
+
+int main(int argc, char **argv)
+{
+	Gpio gpio();
+	/*
+	gpio.pinMode(7, OUTPUT);
+	gpio.digitalWrite(7, HIGH);
+	delay(1000);
+	gpio.digitalWrite(7, LOW);
+	delay(1000);
+	gpio.digitalWrite(7, HIGH);
+	delay(1000);
+	gpio.digitalWrite(7, LOW);
+	delay(1000);
+	*/
+	return 0;
+}
+
