e57ed314b76d7c383fa049cf7139cc5de8ed944b
Fabien Papleux <fabien@papleux.com>
2014-04-02 07:05:54 -0400

    Removing microcontroller directory since its content is now developed elsewhere

---
+++ car/Pin.h
@@ -0,0 +1,21 @@
+/*
+ * Pin.h
+ *
+ *  Created on: Apr 2, 2014
+ *      Author: fabien papleux
+ */
+
+#ifndef PIN_H_
+#define PIN_H_
+
+class Pin
+{
+public:
+
+private:
+};
+
+
+
+
+#endif /* PIN_H_ */
