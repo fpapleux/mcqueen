05f6532a5c238dc2fff6550ee91747bbe6de7ac4
Fabien Papleux <fabien@papleux.com>
2014-07-10 17:00:23 -0400

    atmega328 first test

--- atmega328p/sensing/sensing.ino
+++ atmega328p/sensing/sensing.ino
@@ -5,39 +5,32 @@
  **/
 
 char inCommand [20] = {};   // string to receive commands on the serial line
+int ledRX = 5;
+int ledTX = 6;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(ledRX, OUTPUT);
+  pinMode(ledTX, OUTPUT);
 }
 
 void loop() {
+  int bytesToRead = 0;
+  int incomingByte = 0;
 
+  // Turn off leds
+  digitalWrite (ledRX, LOW);
+  digitalWrite (ledTX, LOW);
+  
   // Read sensors
   
   // Check for serial request
   bytesToRead = Serial.available();
   if (bytesToRead > 0) {
     incomingByte = Serial.read();
-  }
-  if (incomingByte == '0') digitalWrite(13, HIGH);
-  else digitalWrite(13, LOW);
-  delay(1000);
-  Serial.write("pass ");
-}
-
-
-
-/**
- *
- * void getCommand()
- * Loads next command from the serial line into the 'inCommand' char array if available.  Empties the array otherwise.
- *
- **/
-
-void getCommand () {
-  int 
-  int bytesToRead = Serial.available();
-  if (bytesToRead > 0) {
-    while (
+    digitalWrite(ledRX, HIGH);
+    delay(50);
   }
 }
+
+
