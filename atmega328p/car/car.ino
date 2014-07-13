510a1e15ed58a28da73f5ae26a4677c62071a295
Fabien Papleux <fabien@papleux.com>
2014-07-13 14:22:24 -0400

    atmega work

--- atmega328p/car/car.ino
+++ atmega328p/car/car.ino
@@ -15,36 +15,19 @@ void loop()
 
     dir_straight();
     throttle_idle();
+    delay (2000);
     
-    dir_leftPct (25);
-    throttle_forwardPct (25);
-    delay(1000);
     dir_leftPct (50);
-    throttle_forwardPct (50);
-    delay(1000);
-    dir_leftPct (75);
-    throttle_forwardPct (75);
-    delay(1000);
-    dir_leftPct (100);
-    throttle_forwardPct (100);
-    delay(1000);
+    throttle_forwardPct (25);
+    delay (3000);
+
+    dir_straight();
+    throttle_idle();
+    delay (2000);    
   
-    dir_straight ();
-    throttle_idle ();
-    delay(1000);
-  
-    dir_rightPct (25);
-    throttle_reversePct (25);
-    delay(1000);
     dir_rightPct (50);
-    throttle_reversePct (50);
-    delay(1000);
-    dir_rightPct (75);
-    throttle_reversePct (75);
-    delay(1000);
-    dir_rightPct (100);
-    throttle_reversePct (100);
-    delay(1000);
+    throttle_reversePct (25);
+    delay (3000);
 
     i++;
   }
