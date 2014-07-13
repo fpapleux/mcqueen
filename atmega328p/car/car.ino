66864820854198cee6edc83bd4f334f93e511bde
Fabien Papleux <fabien@papleux.com>
2014-07-13 11:48:06 -0400

    atmega work

--- atmega328p/car/car.ino
+++ atmega328p/car/car.ino
@@ -12,13 +12,16 @@ void setup()
 
 void loop()
 {
+  wheel.writeMicroseconds(1500);
+  delay(1500);
   if (i < 4) {
-    wheel.writeMicroseconds(1500);
-    delay(1500);
     wheel.writeMicroseconds(1000);
+    delay(1500);
+    wheel.writeMicroseconds(1500);
     delay(1500);
     wheel.writeMicroseconds(2000);
     delay(1500);
+    i++;
   }
 }
 
